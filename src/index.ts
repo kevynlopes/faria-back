7dccbb31d362ce6b470e5907b1ae8cc96a095ac9
kevynlopes <kevynlopes2019@gmail.com>
2025-05-04 21:27:14 -0300

    change port to run build

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import { createServer } from "./server";
 
 const app = createServer();
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 
 app.listen(PORT, () => {
   console.log(`Servidor rodando na porta ${PORT}`);
